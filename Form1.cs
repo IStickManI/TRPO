7f62354137420e5ee78312c63c1d975ace5d4441
Dmitriy Prihodko <prihodko.dm@mail.ru>
2022-11-13 11:31:21 +0300

    Launcher for Umbrella

---
+++ Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Launcher
+{
+    public partial class Launcher : Form
+    {
+        public Launcher()
+        {
+            
+            InitializeComponent();
+            tabControl1.ItemSize = new Size(tabControl1.Size.Width / tabControl1.TabPages.Count - 1, tabControl1.ItemSize.Height);
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+        }
+    }
+}
